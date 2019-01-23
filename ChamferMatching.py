f03c6e83dcd9cca4eaeb557106152ba82e2987b1
akatsukikaede <genesiswon94@gmail.com>
2019-01-23 11:26:53 +0800

    A simple template matching function is added

--- ChamferMatching.py
+++ ChamferMatching.py
@@ -8,8 +8,13 @@ class ChamferMatching:
     def Eucl_Distance(x1: float, x2: float, y1: float, y2: float) -> float:
         return ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
 
+
+
     # chamfer distance transform, CDT for short
     def Chamfer_Dist_Transform(image: np.ndarray):
+        # two distance value
+        d1 = 1
+        d2 = (d1 * 2) ** 0.5
         # convert pixel data type from int(0-255) to float(0-1.0)
         CDTRes = cv2.normalize(image.astype('float'), None, 0.0, 1.0, cv2.NORM_MINMAX)
         CDTRes[CDTRes!=0]=float("inf")
@@ -26,19 +31,19 @@ class ChamferMatching:
 
                 #pixel 0
                 BWVar0 = CDTRes[i, j - 1]
-                dist0 = ChamferMatching.Eucl_Distance(i, i, j - 1, j) + BWVar0
+                dist0 = d1 + BWVar0
 
                 # pixel 1
                 BWVar1 = CDTRes[i - 1, j - 1]
-                dist1 = ChamferMatching.Eucl_Distance(i - 1, i, j - 1, j) + BWVar1
+                dist1 = d2 + BWVar1
 
                 # pixel 2
                 BWVar2 = CDTRes[i - 1, j]
-                dist2 = ChamferMatching.Eucl_Distance(i - 1, i, j, j) + BWVar2
+                dist2 = d1 + BWVar2
 
                 # pixel 3
                 BWVar3 = CDTRes[i - 1, j + 1]
-                dist3 = ChamferMatching.Eucl_Distance(i - 1, i, j + 1, j) + BWVar3
+                dist3 = d2 + BWVar3
 
                 # min value for all distances
                 fdist = min(BWValCenter, dist0, dist1, dist1, dist2, dist3)
@@ -55,19 +60,19 @@ class ChamferMatching:
 
                 # pixel 4
                 BWVar4 = CDTRes[k, l + 1]
-                dist4 = ChamferMatching.Eucl_Distance(k, k, l + 1, l) + BWVar4
+                dist4 =d1 + BWVar4
 
                 # pixel 5
                 BWVar5 = CDTRes[k + 1, l + 1]
-                dist5 = ChamferMatching.Eucl_Distance(k + 1, k, l + 1, l) + BWVar5
+                dist5 = d2 + BWVar5
 
                 # pixel 6
                 BWVar6 = CDTRes[k + 1, l]
-                dist6 = ChamferMatching.Eucl_Distance(k + 1, k, l, l) + BWVar6
+                dist6 = d1 + BWVar6
 
                 # pixel 7
                 BWVar7 = CDTRes[k + 1, l - 1]
-                dist7 = ChamferMatching.Eucl_Distance(k + 1, k, l - 1, l) + BWVar7
+                dist7 =d2+ BWVar7
 
                 # min dist
                 fdist = min(BWValCenter, dist4, dist5, dist6, dist7)
@@ -78,5 +83,21 @@ class ChamferMatching:
         # normalized output (data type:float)
         CDTRes = cv2.normalize(CDTRes[1:CDTRes.shape[0]-2,1:CDTRes.shape[1] - 2].astype('float'), None, 0.0, 1.0, cv2.NORM_MINMAX)
         return CDTRes
+
+    def MeanConv(image,weight):
+        # size of image and weight
+        height, width = image.shape
+        h, w = weight.shape
+        # size of the result after convolution
+        new_h = height - h + 1
+        new_w = width - w + 1
+        new_image = np.zeros((new_h, new_w), dtype=np.float)
+        # mean convolution
+        for i in range(new_h):
+            for j in range(new_w):
+                new_image[i, j] = (np.sum(image[i:i + h, j:j + w] * weight))/w/h
+                # if(j==new_h-1):
+                #     print("Halt")
+        return new_image
 
 
